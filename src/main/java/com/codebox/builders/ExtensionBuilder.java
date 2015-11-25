f42f13bde15a2079bd4f5dedab9885c3ab42ad18
Jeremy Landis <jeremylandis@hotmail.com>
2015-11-24 22:28:58 -0500

    Cleanup generateGetter/generateSetter

--- src/main/java/com/codebox/builders/ExtensionBuilder.java
+++ src/main/java/com/codebox/builders/ExtensionBuilder.java
@@ -64,20 +64,19 @@ public class ExtensionBuilder<T> {
 
     private static CtMethod generateGetter(CtClass declaringClass, String fieldName, Class<?> fieldClass)
             throws CannotCompileException {
-        final String getterName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
         final StringBuilder sb = new StringBuilder();
-        sb.append("public ").append(fieldClass.getName()).append(" ").append(getterName).append("(){")
+        sb.append("public ").append(fieldClass.getName()).append(" get")
+                .append(fieldName.substring(0, 1).toUpperCase()).append(fieldName.substring(1)).append("(){")
                 .append("return this.").append(fieldName).append(";").append("}");
         return CtMethod.make(sb.toString(), declaringClass);
     }
 
     private static CtMethod generateSetter(CtClass declaringClass, String fieldName, Class<?> fieldClass)
             throws CannotCompileException {
-        final String setterName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
         final StringBuilder sb = new StringBuilder();
-        sb.append("public void ").append(setterName).append("(").append(fieldClass.getName()).append(" ")
-                .append(fieldName).append(")").append("{").append("this.").append(fieldName).append("=")
-                .append(fieldName).append(";").append("}");
+        sb.append("public void set").append(fieldName.substring(0, 1).toUpperCase()).append(fieldName.substring(1))
+                .append("(").append(fieldClass.getName()).append(" ").append(fieldName).append(")").append("{")
+                .append("this.").append(fieldName).append("=").append(fieldName).append(";").append("}");
         return CtMethod.make(sb.toString(), declaringClass);
     }
 
