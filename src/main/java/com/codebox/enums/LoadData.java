40aaa2f602f4bbae3dd21acd063e6cfb625d84ca
Jeremy Landis <jeremylandis@hotmail.com>
2017-04-24 21:53:16 -0400

    [ci] Update copyright to 2017

--- src/main/java/com/codebox/enums/LoadData.java
+++ src/main/java/com/codebox/enums/LoadData.java
@@ -1,7 +1,7 @@
 /**
  * JavaBean Tester (https://github.com/hazendaz/javabean-tester)
  *
- * Copyright (c) 2012 - 2015 Hazendaz.
+ * Copyright (c) 2012 - 2017 Hazendaz.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of The Apache Software License,
