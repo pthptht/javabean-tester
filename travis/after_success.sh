ce792a43346b05e267d6e4d2398cd50236ec2c60
Jeremy Landis <jeremylandis@hotmail.com>
2017-05-26 16:45:02 -0400

    [ci] Update after_success script

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# Get Project Repo
-upstream_repo=$(git config --get remote.origin.url 2>&1)
-echo "Repo detected: ${upstream_repo}"
- 
-# Get the Java version.
-# Java 1.6 will give 16.
-# Java 1.7 will give 17.
-# Java 1.8 will give 18.
-VER=`java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q'`
-echo "Java detected: ${VER}"
+# Get Commit Message
+commit_message=$(git log --format=%B -n 1)
+echo "Current commit detected: ${commit_message}"
 
 # We build for several JDKs on Travis.
 # Some actions, like analyzing the code (Coveralls) and uploading
@@ -21,24 +14,32 @@ echo "Java detected: ${VER}"
 #    b. Use --settings to force the usage of our "settings.xml" file.
 # 2. Notify Coveralls.
 #    a. Use -q option to only display Maven errors and warnings.
+#    b. Use --settings to force the usage of our "settings.xml" file.
 # 3. Deploy site
 #    a. Use -q option to only display Maven errors and warnings.
 #    b. Use --settings to force the usage of our "settings.xml" file.
 
-if [ "$upstream_repo" == "https://github.com/hazendaz/javabean-tester.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [ $VER == "18" ]; then
-    # Send snapshots to sonatype
-    mvn clean deploy -q --settings ./travis/settings.xml
+if [ $TRAVIS_REPO_SLUG == "hazendaz/javabean-tester" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
+
+  if [ ${TRAVIS_JDK_VERSION} == "oraclejdk8" ]; then
+
+    # Deploy snapshot to sonatype
+    ./mvnw clean deploy -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 
-	# Send coverate to coveralls
-    mvn clean test jacoco:report coveralls:report -q
+	# Deploy coverage to coveralls
+    ./mvnw clean test jacoco:report coveralls:report -q --settings ./travis/settings.xml
     echo -e "Successfully ran coveralls under Travis job ${TRAVIS_JOB_NUMBER}"
 
-	# various issues exist currently in building this so comment for now
-	mvn site site:deploy -q --settings ./travis/settings.xml
-	echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
+    # Deploy site to ghpages
+	# Cannot currently run site this way
+	# ./mvnw site site:deploy -q --settings ./travis/settings.xml
+	# echo -e "Successfully deploy site under Travis job ${TRAVIS_JOB_NUMBER}"
+  else
+    echo "Java Version does not support additonal activity for travis CI"
   fi
 else
+  echo "Travis Pull Request: $TRAVIS_PULL_REQUEST"
+  echo "Travis Branch: $TRAVIS_BRANCH"
   echo "Travis build skipped"
 fi
