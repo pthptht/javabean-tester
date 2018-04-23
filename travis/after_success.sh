db78f7babdf6c0b6b4fe704d777c4e0e1a7f043f
Jeremy Landis <jeremylandis@hotmail.com>
2018-04-22 20:54:53 -0400

    [license] Fix license config and license other files

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+#
+# JavaBean Tester (https://github.com/hazendaz/javabean-tester)
+#
+# Copyright (c) 2012 - 2017 Hazendaz.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of The Apache Software License,
+# Version 2.0 which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Contributors:
+#     CodeBox (Rob Dawson).
+#     Hazendaz (Jeremy Landis).
+#
+
 
 # Get Commit Message
 commit_message=$(git log --format=%B -n 1)
