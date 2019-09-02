1d7fc2877f87465a786466733054642b42cb671e
Jeremy Landis <jeremylandis@hotmail.com>
2019-09-02 15:05:30 -0400

    [license] Use more standard style / update dates

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -2,7 +2,7 @@
 #
 # JavaBean Tester (https://github.com/hazendaz/javabean-tester)
 #
-# Copyright (c) 2012 - 2018 Hazendaz.
+# Copyright 2012-2019 Hazendaz.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of The Apache Software License,
